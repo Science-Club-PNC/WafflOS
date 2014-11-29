a24c12a8e556d0973da86ab75d95022d520213b7
mid-kid <esteve.varela@gmail.com>
2014-11-29 19:18:55 +0100

    Fixing indentation and trailing... tabs?
    Steven, configureer je editor om 4 spaties te gebruiken in plaats van tabs. Laat ook geen onnodige spaties achter statements. Tnx.

--- kernel/main.c
+++ kernel/main.c
@@ -6,8 +6,8 @@ void main()
     terminal_reset();
     for (int i = 0; i < 10; i++) {
         terminal_setcolor(i % 15, i % 30 / 2);
-	char number[5] = { 0 };
-	terminal_writestring(int_to_string(i, number, 5));
+        char number[5] = { 0 };
+        terminal_writestring(int_to_string(i, number, 5));
         terminal_writestring(" lines: ");
         for (int x = 0; x < i; x++) {
             terminal_writechar('|');
--- libc/free/string.c
+++ libc/free/string.c
@@ -4,36 +4,36 @@
 
 char* string_reverse(char* target)
 {
-	size_t len = strlen(target);	
-	char* i = target;
-	char* j = target + len -1;
-	
-	while (i < j) {
-		char temp = *i;	
-		*i = *j;
-		*j = temp;
-		i++;
-		j--;
-	}
+    size_t len = strlen(target);
+    char* i = target;
+    char* j = target + len -1;
 
-	return target;
+    while (i < j) {
+        char temp = *i;
+        *i = *j;
+        *j = temp;
+        i++;
+        j--;
+    }
+
+    return target;
 }
 
 char* int_to_string(int a, char* target, size_t len)
 {
-	size_t i;
-	for (i = 0; i < len - 1; i++) {
-		target[i] = (char)(a % 10) + '0';
-		a /= 10;
-		if (a == 0) {
-			i++;
-			break;
-		}
-	}	
+    size_t i;
+    for (i = 0; i < len - 1; i++) {
+        target[i] = (char)(a % 10) + '0';
+        a /= 10;
+        if (a == 0) {
+            i++;
+            break;
+        }
+    }
 
-	target[i] = '\0';
+    target[i] = '\0';
 
-	return string_reverse(target);
+    return string_reverse(target);
 }
 
 size_t strlen(char *str)
