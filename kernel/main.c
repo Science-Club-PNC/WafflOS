462e5776f5001557643ab4aed5d0009c47025e1d
mid-kid <esteve.varela@gmail.com>
2014-11-29 19:26:09 +0100

    Waffles of juice

--- kernel/main.c
+++ kernel/main.c
@@ -4,14 +4,17 @@
 void main()
 {
     terminal_reset();
-    for (int i = 0; i < 10; i++) {
+    char amount_of_waffles[3];
+    for (int i = 99; i > 0; i--) {
         terminal_setcolor(i % 15, i % 30 / 2);
-        char number[5] = { 0 };
-        terminal_writestring(int_to_string(i, number, 5));
-        terminal_writestring(" lines: ");
-        for (int x = 0; x < i; x++) {
-            terminal_writechar('|');
-        }
+        terminal_writestring(int_to_string(i, amount_of_waffles, 3));
+        terminal_writestring(" waffles of juice on the wall, ");
+        terminal_writestring(amount_of_waffles);
+        terminal_writestring(" waffles of juice. Take one down, and pass it around, ");
+        terminal_writestring(amount_of_waffles);
+        terminal_writestring(" waffles of juice on the wall.");
         terminal_writechar('\n');
     }
+    terminal_setcolor(color_white, color_black);
+    terminal_writestring("No more waffles of juice on the wall, no more waffles of juice. Go to the store, and buy some more. 99 waffles of juice on the wall.");
 }
