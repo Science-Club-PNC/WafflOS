44776b85eeb0bc9e9e48517a42d749c10e923f3a
mid-kid <esteve.varela@gmail.com>
2014-12-02 17:08:03 +0100

    printf() works... ish.

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,13 @@
 #include <string.h>
+#include <io.h>
 #include "terminal.h"
 #include "malloc.h"
 
 void main()
 {
 	terminal_reset();
-	init_mem();	
-	test_mem();	
+    printf("Waffles\n");
+    printf("%caffles with %s\n", 'W', "cheese");
+    printf("%i waffles in a jar of %i inch\n", 99, 53);
+    printf("Wüfflüss\n");
 }
---
+++ libc/free/io.c
@@ -0,0 +1,41 @@
+#include <kernel/terminal.h>
+
+#include "vararg.h"
+#include "string.h"
+
+#include "io.h"
+
+#define max_int 10
+
+int printf(char *string, ...)
+{
+    va_list *args;
+    char i[max_int];
+    va_start(args, string);
+
+    size_t pos;
+    for (pos = 0; string[pos]; pos++) {
+        if (string[pos] == '%') {
+            switch (string[++pos]) {
+                case 's':
+                    terminal_writestring(va_arg(args, char *));
+                    break;
+                case 'c':
+                    terminal_writechar(va_arg(args, char));
+                    break;
+                case 'i':
+                    terminal_writestring(int_to_string(va_arg(args, int), i, max_int));
+                    break;
+                case '%':
+                    terminal_writechar('%');
+                    break;
+                default:
+                    return -1;
+            }
+        } else {
+            terminal_writechar(string[pos]);
+        }
+    }
+
+    return pos;
+}
