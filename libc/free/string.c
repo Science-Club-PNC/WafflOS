dedf1a0d709cdfa5d503a9ac6e4f8ef943f755ae
buildersteven4 <buildersteven4@gmail.com>
2014-12-08 21:22:20 +0100

    Added memset function to string.h

--- libc/free/string.c
+++ libc/free/string.c
@@ -12,8 +12,8 @@ void* memmove(void* dst_ptr, const void* src_ptr, size_t size)
     if (dst_ptr < src_ptr) {
         const unsigned char* src_end = (const unsigned char*)src_ptr + size;
 
-        dst = (unsigned char*) dst_ptr;
-        src = (const unsigned char*) src_ptr;
+        dst = (unsigned char*)dst_ptr;
+        src = (const unsigned char*)src_ptr;
 
         while (src < src_end) {
             *dst = *src;
@@ -23,8 +23,8 @@ void* memmove(void* dst_ptr, const void* src_ptr, size_t size)
     } else {
         const unsigned char* src_start = (const unsigned char*)src_ptr;
 
-        dst = (unsigned char*) dst_ptr + size - 1;
-        src = (const unsigned char*) src_ptr + size - 1;
+        dst = (unsigned char*)dst_ptr + size - 1;
+        src = (const unsigned char*)src_ptr + size - 1;
 
         while (src >= src_start) {
             *dst = *src;
@@ -39,8 +39,8 @@ void* memmove(void* dst_ptr, const void* src_ptr, size_t size)
 //TODO: write word alignment optimalization
 void* memcpy(void* dst_ptr, const void* src_ptr, size_t size)
 {
-    unsigned char* dst = (unsigned char*) dst_ptr;
-    const unsigned char* src = (const unsigned char*) src_ptr;
+    unsigned char* dst = (unsigned char*)dst_ptr;
+    const unsigned char* src = (const unsigned char*)src_ptr;
 
     const unsigned char* src_end = (const unsigned char*)src_ptr + size;
 
@@ -50,6 +50,21 @@ void* memcpy(void* dst_ptr, const void* src_ptr, size_t size)
         dst++;
     }
 
+    return dst_ptr;
+}
+
+void* memset(void* dst_ptr, int src, size_t size)
+{
+    const unsigned char source = (const unsigned char)src;
+    
+    unsigned char* dst = (unsigned char*)dst_ptr;
+    const unsigned char* dst_end = dst_ptr + size;
+
+    while (dst < dst_end) {
+        *dst = source;
+        dst++;
+    }
+    
     return dst_ptr;
 }
 
--- libc/free/string.h
+++ libc/free/string.h
@@ -5,6 +5,7 @@
 
 void* memmove(void* dst_ptr, const void* src_ptr, size_t size);
 void* memcpy(void* dst_ptr, const void* src_ptr, size_t size);
+void* memset(void* dst_ptr, int source, size_t size); 
 
 char *string_reverse(char* target, size_t len);
 char *int_to_string(int a, char* target, size_t len);
