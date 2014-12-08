da8f72acb6cd95b8d85ad717f44f2582a25fb719
buildersteven4 <buildersteven4@gmail.com>
2014-12-08 16:30:42 +0100

    forgot to add memcpy to the sting header... oops :3

--- libc/free/string.h
+++ libc/free/string.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 void* memmove(void* dst_ptr, const void* src_ptr, size_t size);
+void* memcpy(void* dst_ptr, const void* src_ptr, size_t size);
 
 char *string_reverse(char* target, size_t len);
 char *int_to_string(int a, char* target, size_t len);
