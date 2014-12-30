af0c283331e06302f2bb9ea94b61605358836a79
mid-kid <esteve.varela@gmail.com>
2014-12-30 18:40:42 +0100

    Derp.

--- libc/free/string.h
+++ libc/free/string.h
@@ -2,18 +2,13 @@
 #define __string_h__
 
 #include <stddef.h>
-#include <bool.h>
+#include <stdbool.h>
 
 size_t strlen(char* str);
 char* string_reverse(char* target, size_t len);
 
 char* to_dec_string(void b, char* target, size_t len, bool sign);
-#define char_to_dec_string(a, target, len) to_dec_string((void)a, target, len, false)
-#define short_to_dec_string(a, target, len) to_dec_string((void)a, target, len, true)
 #define int_to_dec_string(a, target, len) to_dec_string((void)a, target, len, true)
-#define long_to_dec_string(a, target, len) to_dec_string((void)a, target, len, true)
-#define ushort_to_dec_string(a, target, len) to_dec_string((void)a, target, len, false)
 #define uint_to_dec_string(a, target, len) to_dec_string((void)a, target, len, false)
-#define ulong_to_dec_string(a, target, len) to_dec_string((void)a, target, len, false)
 
 #endif
