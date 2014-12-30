45d7369453e3a5e1ab9bb55f7b459a910f9b62c1
mid-kid <esteve.varela@gmail.com>
2014-12-30 17:45:19 +0100

    Proposal to fix the mess of '*_to_dec_string'

--- libc/free/string.c
+++ libc/free/string.c
@@ -28,63 +28,35 @@ char* string_reverse(char* target, size_t len)
 }
 
 //TODO: Don't depend on len
-#define INT_TO_DEC_STRING_CODE {            \
-    bool negative_input = false;            \
-    if (a<0) {                              \
-        a = -a;                             \
-        negative_input = true;              \
-    }                                       \
-                                            \
-    size_t i;                               \
-    for (i = 0; i < len - 1; i++) {         \
-        int extract = a % 10;               \
-        target[i] = (char)extract + '0';    \
-        a = (a - extract) / 10;             \
-        if (a == 0) {                       \
-            i++;                            \
-            break;                          \
-        }                                   \
-    }                                       \
-    if (negative_input && (i < len - 1)) {  \
-        target[i] = '-';                    \
-        i++;                                \
-    }                                       \
-    target[i] = '\0';                       \
-                                            \
-    return string_reverse(target, i);       \
+char* to_dec_string(void b, char* target, size_t len, bool sign)
+{
+    if (sign) {
+        long a = b;
+    } else {
+        unsigned long a = b;
+    }
+
+    bool negative_input = false;
+    if (a < 0 && sign) {
+        a = -a;
+        negative_input = true;
+    }
+
+    size_t i;
+    for (i = 0; i < len - 1; i++) {
+        int extract = a % 10;
+        target[i] = (char)extract + '0';
+        a = (a - extract) / 10;
+        if (a == 0) {
+            i++;
+            break;
+        }
+    }
+    if (negative_input && (i < len - 1)) {
+        target[i] = '-';
+        i++;
+    }
+    target[i] = '\0';
+
+    return string_reverse(target, i);
 }
-
-#define UINT_TO_DEC_STRING_CODE {           \
-    size_t i;                               \
-    for (i = 0; i < len - 1; i++) {         \
-        int extract = a % 10;               \
-        target[i] = (char)extract + '0';    \
-        a = (a - extract) / 10;             \
-        if (a == 0) {                       \
-            i++;                            \
-            break;                          \
-        }                                   \
-    }                                       \
-    target[i] = '\0';                       \
-                                            \
-    return string_reverse(target, i);       \
-}
-
-// Code assumes that char is unsigned
-char* char_to_dec_string(unsigned char a, char* target, size_t len)
-UINT_TO_DEC_STRING_CODE
-
-char* short_to_dec_string(short a, char* target, size_t len)
-INT_TO_DEC_STRING_CODE
-char* int_to_dec_string(int a, char* target, size_t len)
-INT_TO_DEC_STRING_CODE
-char* long_to_dec_string(long a, char* target, size_t len)
-INT_TO_DEC_STRING_CODE
-
-char* ushort_to_dec_string(unsigned short a, char* target, size_t len)
-UINT_TO_DEC_STRING_CODE
-char* uint_to_dec_string(unsigned int a, char* target, size_t len)
-UINT_TO_DEC_STRING_CODE
-char* ulong_to_dec_string(unsigned long a, char* target, size_t len)
-UINT_TO_DEC_STRING_CODE
-
--- libc/free/string.h
+++ libc/free/string.h
@@ -2,18 +2,18 @@
 #define __string_h__
 
 #include <stddef.h>
+#include <bool.h>
 
 size_t strlen(char* str);
 char* string_reverse(char* target, size_t len);
 
-char* char_to_dec_string(unsigned char a, char* target, size_t len);
-
-char* short_to_dec_string(short a, char* target, size_t len);
-char* int_to_dec_string(int a, char* target, size_t len);
-char* long_to_dec_string(long a, char* target, size_t len);
-
-char* ushort_to_dec_string(unsigned short a, char* target, size_t len);
-char* uint_to_dec_string(unsigned int a, char* target, size_t len);
-char* ulong_to_dec_string(unsigned long a, char* target, size_t len);
+char* to_dec_string(void b, char* target, size_t len, bool sign);
+#define char_to_dec_string(a, target, len) to_dec_string((void)a, target, len, false)
+#define short_to_dec_string(a, target, len) to_dec_string((void)a, target, len, true)
+#define int_to_dec_string(a, target, len) to_dec_string((void)a, target, len, true)
+#define long_to_dec_string(a, target, len) to_dec_string((void)a, target, len, true)
+#define ushort_to_dec_string(a, target, len) to_dec_string((void)a, target, len, false)
+#define uint_to_dec_string(a, target, len) to_dec_string((void)a, target, len, false)
+#define ulong_to_dec_string(a, target, len) to_dec_string((void)a, target, len, false)
 
 #endif
