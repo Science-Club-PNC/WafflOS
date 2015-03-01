2d91bdace0d6c67ac09a9bb2e14b1664da3cad03
buildersteven4 <buildersteven4@gmail.com>
2015-03-01 23:10:01 +0100

    Refined event_handling.c for a bit

--- kernel/main/event_handling.c
+++ kernel/main/event_handling.c
@@ -4,19 +4,26 @@
 
 #include "main.h"
 #include "../event.h"
+#include "../abort.h"
 #include "../keyboard/keys.h"
+
+static void handle_keyboard_event(keyboard_event* event)
+{
+    if (!event->key_was_released) {
+        printf("%c", event->character);
+    }
+}
 
 void kernel_handle_events()
 {
     event_list events = get_events();
     for (size_t event_num = 0; event_num < events.length; event_num++) {
         switch (events.events[event_num].type) {
-            case event_type_keyboard: {
-                keyboard_event* event = (keyboard_event*)&events.events[event_num];
-                if (!event->key_was_released) {
-                    printf("%c", event->character);
-                }
-            }
+            case event_type_keyboard:
+                handle_keyboard_event((keyboard_event*)&events.events[event_num]);
+                break;
+            default:
+                abort("unkown event found");
         }
     }
 }
